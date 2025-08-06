8d7560c9d158fe6842335e64d27b3210361c053e
Zach Wei <wyf09140@gmail.com>
2025-08-06 12:34:32 -0700

    Lab5 final update

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -14,7 +14,11 @@ const app = express();
 // ✅ CORS 必须允许凭证，并且 origin 必须与前端一致
 app.use(cors({
   credentials: true,
-  origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  origin: [
+    "https://a4--cosmic-pithivier-a2929c.netlify.app",
+    "https://a5--cosmic-pithivier-a2929c.netlify.app",
+    "http://localhost:5173"
+  ]
 }));
 
 // ✅ 解析 JSON —— 放在路由之前
