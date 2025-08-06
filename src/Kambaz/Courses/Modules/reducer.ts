08260459213a00243f820ba701d26ebf944bb0fd
Zach Wei <wyf09140@gmail.com>
2025-08-06 11:53:28 -0700

    Lab5 final update

--- src/Kambaz/Courses/Modules/reducer.ts
+++ src/Kambaz/Courses/Modules/reducer.ts
@@ -1,6 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { v4 as uuidv4 } from "uuid";
-const initialState = {
+
+export interface Module {
+  _id: string;
+  name: string;
+  course: string;
+  lessons: any[];
+  editing?: boolean;
+}
+
+interface ModulesState {
+  modules: Module[];
+}
+
+const initialState: ModulesState = {
   modules: [],
 };
 
@@ -8,36 +20,34 @@ const modulesSlice = createSlice({
   name: "modules",
   initialState,
   reducers: {
-
     setModules: (state, action) => {
       state.modules = action.payload;
     },
-
-    addModule: (state, { payload: module }) => {
-      const newModule: any = {
-        _id: uuidv4(),
-        lessons: [],
-        name: module.name,
-        course: module.course,
-      };
-      state.modules = [...state.modules, newModule] as any;
+    addModule: (state, action) => {
+      state.modules.push(action.payload);
     },
-    deleteModule: (state, { payload: moduleId }) => {
-      state.modules = state.modules.filter(
-        (m: any) => m._id !== moduleId);
+    deleteModule: (state, action) => {
+      state.modules = state.modules.filter((m) => m._id !== action.payload);
     },
-    updateModule: (state, { payload: module }) => {
-      state.modules = state.modules.map((m: any) =>
-        m._id === module._id ? module : m
-      ) as any;
+    updateModule: (state, action) => {
+      state.modules = state.modules.map((m) =>
+        m._id === action.payload._id ? action.payload : m
+      );
     },
-    editModule: (state, { payload: moduleId }) => {
-      state.modules = state.modules.map((m: any) =>
-        m._id === moduleId ? { ...m, editing: true } : m
-      ) as any;
+    editModule: (state, action) => {
+      state.modules = state.modules.map((m) =>
+        m._id === action.payload ? { ...m, editing: true } : m
+      );
     },
   },
 });
-export const {setModules, addModule, deleteModule, updateModule, editModule } =
-  modulesSlice.actions;
+
+export const {
+  setModules,
+  addModule,
+  deleteModule,
+  updateModule,
+  editModule,
+} = modulesSlice.actions;
+
 export default modulesSlice.reducer;
